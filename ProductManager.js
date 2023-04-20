caa354b13a2348772a7e8ba5df5d8288688d57ab
Mauro notebook <exalted.archer@gmail.com>
2023-04-20 13:34:32 -0300

    solución async console log en getProducts

--- ProductManager.js
+++ ProductManager.js
@@ -3,8 +3,6 @@
 //importar fileSystem
 const fs = require('fs');
 
-
-
 //Creo la clase ProductManager
 class ProductManager {
     constructor (){
@@ -13,11 +11,10 @@ class ProductManager {
         this. incrementalId = 0
     }
 
-//metodo addProduct
-addProduct = async ( title, description, price, thumbnail, code, stock) => {
+    //metodo addProduct
+    addProduct = async ( title, description, price, thumbnail, code, stock) => {
 
    this.incrementalId++;
-
    let newProduct = {
     title,
     description,
@@ -27,41 +24,28 @@ addProduct = async ( title, description, price, thumbnail, code, stock) => {
     stock,
     id: this.incrementalId,
    }
-    
-//uso this.products para almacenar los diferentes push que genere el addProduct
-this.products.push (newProduct);
+    //uso this.products para almacenar los diferentes push que genere el addProduct
+    this.products.push (newProduct);
 
-
-
- await fs.promises.writeFile (this.path , JSON.stringify (this.products))
+    await fs.promises.writeFile (this.path , JSON.stringify (this.products))
 } 
 
-
-//metodo getProducts
-
-getProducts = async() => {
-  let readProducts =  await fs.promises.readFile (this.path, "utf-8");
-  let readProductsParsed = JSON.parse (readProducts);
-  console.log (readProductsParsed);
+    //metodo getProducts
+    getProducts = async() => {
+    let readProducts =  await fs.promises.readFile (this.path, "utf-8");
+    let readProductsParsed = await JSON.parse (readProducts); // uso await acá porque tengo problemas de sincronía en el console.log
+    console.log (readProductsParsed);
+    }
 }
 
-
-}
-
-
-
-
-/// Instanciar const productManager y comprobaciones
-
+// Instanciar const productManager y comprobaciones
 const productManager = new ProductManager;
 
-
 //compruebo que addproduct crea y escribe el documento txt
- productManager.addProduct ("titulo 1","descripcion 1",100,"imagen 1","abc123", 20);
+productManager.addProduct ("titulo 1","descripcion 1",100,"imagen 1","abc123", 20);
 
- //agrego el 2do producto para comprobar que se agrega y no sobreescribe al producto 1
-
- productManager.addProduct ("titulo 2","descripcion 2",100,"imagen 2","abc124", 20);
+//agrego el 2do producto para comprobar que se agrega y no sobreescribe al producto 1
+productManager.addProduct ("titulo 2","descripcion 2",100,"imagen 2","abc124", 20);
 
 //compruebo que getProducts y JSON.parse funcionen 
- productManager.getProducts ();
+productManager.getProducts ();
